a191e226823c3a2327ba8f384a34e30adee70508
adiyakl <adiyakl2006@gmail.com>
2024-01-10 17:49:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/timerpicker/AlarmReceiver.java
@@ -0,0 +1,17 @@
+
+package com.example.timerpicker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent ri) {
+        String msg = ri.getStringExtra("msg");
+        Toast.makeText(context, msg+" Alarm has been activated !", Toast.LENGTH_LONG).show();
+        }
+}
+
---
+++ app/src/main/java/com/example/timerpicker/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.timerpicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
+
+import android.app.AlarmManager;
+
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button timeButton;
+    private TextView tv;
+
+    private int hour , minute;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+    private int ALARM_RQST_CODE=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timeButton = findViewById(R.id.timebt);
+        tv= findViewById(R.id.textView);
+    }
+
+    public void popTimePicker(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+            hour = selectedHour;
+            minute = selectedMinute;
+            timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour, minute));
+            }
+        };
+        int style = AlertDialog.BUTTON_NEUTRAL;
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, /*style,*/ onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+
+
+
+
+
+
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            Calendar calNow = Calendar.getInstance();
+            Calendar calSet = (Calendar) calNow.clone();
+
+            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calSet.set(Calendar.MINUTE, minute);
+            calSet.set(Calendar.SECOND, 0);
+            calSet.set(Calendar.MILLISECOND, 0);
+
+            if (calSet.compareTo(calNow) <= 0) {
+                calSet.add(Calendar.DATE, 1);
+            }
+            setAlarm(calSet);
+        }
+    };
+
+    public void todAlarm(View view) {
+        openTimePickerDialog(true);
+    }
+    private void openTimePickerDialog(boolean is24r) {
+        Calendar calendar = Calendar.getInstance();
+        Date currentDate = calendar.getTime();
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE), is24r);
+        timePickerDialog.setTitle("Choose time");
+        timePickerDialog.show();
+    }
+
+    private void setAlarm(Calendar calSet) {
+        ALARM_RQST_CODE++;
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        intent.putExtra("msg", String.valueOf(ALARM_RQST_CODE) + " TOD");
+        alarmIntent = PendingIntent.getBroadcast(this,
+                ALARM_RQST_CODE, intent, PendingIntent.FLAG_IMMUTABLE);
+        alarmMgr = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.set(AlarmManager.RTC_WAKEUP,
+                calSet.getTimeInMillis(), alarmIntent);
+        tv.setText(String.valueOf(ALARM_RQST_CODE) + " Alarm in " + String.valueOf(calSet.getTime()));
+    }
+
+//    public void cancelAlarm(View view) {
+//        Intent intent = new Intent(this, AlarmReceiver.class);
+//        alarmIntent = PendingIntent.getBroadcast(this,
+//
+//                ALARM_RQST_CODE, intent, PendingIntent.FLAG_IMMUTABLE);
+//        alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+//        alarmMgr.cancel(alarmIntent);
+//        tV.setText("Alarm "+String.valueOf(ALARM_RQST_CODE)+" canceled!");
+//        ALARM_RQST_CODE--;
+//    }
+//    public void popTimePicker(View view) {
+//        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+//            @Override
+//            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+//            hour = selectedHour;
+//            minute = selectedMinute;
+//            timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour, minute));
+//            }
+//        };
+//        int style = AlertDialog.BUTTON_NEUTRAL;
+//        TimePickerDialog timePickerDialog = new TimePickerDialog(this, /*style,*/ onTimeSetListener, hour, minute, true);
+//
+//        timePickerDialog.setTitle("Select Time");
+//        timePickerDialog.show();
+//    }
+    }
+
